36dd744016701607955367f438f5c936c5e62635
Matias Vespa <matty9706@gmail.com>
2025-06-24 18:05:03 -0300

    Servicios retoques

--- js/servicios-data.js
+++ js/servicios-data.js
@@ -6,21 +6,21 @@ export const serviciosIniciales = [
         nombre: 'Servicio de DJ Profesional',
         descripcion: 'La mejor música para tu evento. Nuestros DJs cuentan con una amplia variedad de géneros musicales y equipos de sonido e iluminación de última generación para crear el ambiente perfecto.',
         imagen: 'https://placehold.co/600x400/343a40/ffffff?text=DJ',
-        precio: 15000,
+        precio: 75000,
     },
     {
         id: 2,
         nombre: 'Catering Exclusivo',
         descripcion: 'Deleita a tus invitados con nuestras propuestas gastronómicas. Ofrecemos desde cócteles y recepciones hasta menús completos de varios pasos, adaptados a tus gustos y necesidades.',
         imagen: 'https://placehold.co/600x400/007bff/ffffff?text=Catering',
-        precio: 15000,
+        precio: 97600,
     },
     {
         id: 3,
         nombre: 'Animación y Entretenimiento',
         descripcion: 'Shows en vivo, magos, animadores y mucho más para que tu evento sea único y memorable. Coordinamos el entretenimiento ideal para todo tipo de celebración, tanto para adultos como para niños.',
         imagen: 'https://placehold.co/600x400/28a745/ffffff?text=Show',
-        precio: 15000,
+        precio: 55260,
     }
 ];
 
